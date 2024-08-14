0780bf329a254cac4b939e5fbdd71731a759cd26
Guilherme Domenes Santana <guilherme.santana@clude.com.br>
2024-08-14 12:28:30 -0300

    att

--- novo-portifolio.js
+++ novo-portifolio.js
@@ -143,130 +143,151 @@ const empregosDiv = document.querySelector('.empregos')
 
     /******************************************************** */
   /**************   SITES FEITOS   *************** */
-  /*WORDPRESS
 
-https://safras.com.br/
-
-https://agenciacma.com.br/
-
-https://www.cma.com.br/
-
-https://irani.com.br/
-
-https://www.restaurabrasil.org.br/
-
-https://www.maiscarnesuina.com.br/
-
-https://cromasolutions.com.br/
-
-https://razoesparaacreditar.com/
-
-https://todaycorp.com.br/
-
-https://4healthalphaville.com.br
-
-https://escolasuperioresn.com.br
-
-https://petworks.com.br
-
-https://brasports.com.br
-
-https://mbaeachusp.com
-
-https://longevitech.com.br
-
-https://www.agenciapalmtree.com/
-
-MAGENTO
-
-https://uselina.com.br
-
-https://www.mapuff.com
-
-WIX
-
-https://www.clinicadvance.com.br/ */
     const menu = [
       {
         id: 1,
         title: "Roberta Faustini",
         category: "Ecommerce",
         price: 15.99,
-        img: "img/item-1.jpeg",
-        link: "https://agenciapalmtree.com",
-        desc: `Ecommerce feito em Woocommerce, integrado com Bling e Jetlog`,
+        img: "https://robertasafaustini.com.br/wp-content/uploads/2022/04/LOGO_robertasafaustini_capa-1-scaled-1.jpg",
+        link: "https://robertasafaustini.com.br/shop/",
+        desc: `Criado em Wordpress, esta loja faz a venda de semi joias.`,
       },
       {
         id: 2,
         title: "Agencia Palmtree",
         category: "Site",
         price: 13.99,
-        img: "img/item-2.jpeg",
+        img: "https://www.agenciapalmtree.com/wp-content/uploads/2024/04/logo-palm-branco.webp",
         link: "https://agenciapalmtree.com",
-        desc: `Site para uma agência de marketing, utilizando SEO para captação de leads de forma organica`,
+        desc: `Criado em wordpress, para uma agência de marketing, com foco em SEO, para captar leads.`,
       },
       {
         id: 3,
-        title: "godzilla milkshake",
-        category: "shakes",
+        title: "Longevitech",
+        category: "Ecommerce",
         price: 6.99,
-        img: "img/item-3.jpeg",
-        link: "https://agenciapalmtree.com",
-        desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+        img: "https://longevitech.com.br/wp-content/uploads/2023/06/Logotipo-Degrade-1400x248.png",
+        link: "https://longevitech.com.br",
+        desc: `Criado em Wordpress, especializado em produtos inteligentes para uma vida melhor com soluções inovadoras em Tecnologia Assistiva que ampliem a independência do indivíduo.`,
       },
       {
         id: 4,
-        title: "country delight",
-        category: "breakfast",
+        title: "MBA EACH USP",
+        category: "Site",
         price: 20.99,
-        img: "img/item-4.jpeg",
-        link: "https://agenciapalmtree.com",
-        desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+        img: "https://mbaeachusp.com/wp-content/uploads/2022/09/Logo-Completo-Branco.png",
+        link: "https://mbaeachusp.com",
+        desc: `Site criado em wordpress, para captura de leads interessadas em cursos MBA.`,
       },
       {
         id: 5,
-        title: "egg attack",
-        category: "lunch",
+        title: "BRA Sports",
+        category: "Site",
         price: 22.99,
-        img: "img/item-5.jpeg",
-        link: "https://agenciapalmtree.com",
-        desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+        img: "https://brasports.com.br/wp-content/uploads/2019/07/bra-400.png",
+        link: "https://brasports.com.br",
+        desc: `Criado em wordpress, este site tem como principal objetivo, o agendamento de quadras esportivas.`,
       },
       {
         id: 6,
-        title: "oreo dream",
-        category: "shakes",
+        title: "Petworks",
+        category: "Ecommerce",
         price: 18.99,
-        img: "img/item-6.jpeg",
-        link: "https://agenciapalmtree.com",
-        desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+        img: "https://petworks.com.br/wp-content/uploads/2022/08/pet-works-branco-marrom-1024x601.png",
+        link: "https://petworks.com.br/",
+        desc: `Criado em Wordpress, este site tem como principal, a divulgação dos produtos para pets, lançados pela empresa Petworks.`, 
       },
       {
         id: 7,
-        title: "bacon overflow",
-        category: "breakfast",
+        title: "Escola Superior Contabil Fiscal e RH",
+        category: "Ecommerce",
         price: 8.99,
-        img: "img/item-7.jpeg",
-        link: "https://agenciapalmtree.com",
-        desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+        img: "https://escolasuperioresn.com.br/wp-content/uploads/2023/09/logo-site.png",
+        link: "https://escolasuperioresn.com.br",
+        desc: `Criado em wordpress, este ecommerce faz vendas de cursos para as areas contábil, fiscal e RH.`,
       },
       {
         id: 8,
-        title: "american classic",
-        category: "lunch",
+        title: "4 Health Alphaville",
+        category: "Site",
         price: 12.99,
-        img: "img/item-8.jpeg",
-        link: "https://agenciapalmtree.com",
-        desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
+        img: "https://4healthalphaville.com.br/wp-content/uploads/2024/03/Logo-4Health-qiquedk1fjzmybpb671hp39wxbpbojp50797l74kbg.webp",
+        link: "https://4healthalphaville.com.br",
+        desc: `Criado em Wordpress, com a meta de divulgar os serviços prestados na clinica.`,
       },
       {
         id: 9,
-        title: "quarantine buddy",
-        category: "shakes",
+        title: "Today Corp",
+        category: "Site",
         price: 16.99,
-        img: "img/item-9.jpeg",
-        link: "https://agenciapalmtree.com",
-        desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+        img: "https://www.todaycorp.com.br/wp-content/uploads/2023/04/TodayCorp-Logo-brancoamarelo-1024x573.png",
+        link: "https://todaycorp.com.br/",
+        desc: `Criado em Wordpress, para dilvulgar a marca e os produtos prestados por esta empresa de Eventos.`,
+      },
+      {
+        id: 10,
+        title: "Mais Carne Suína",
+        category: "Ecommerce",
+        price: 16.99,
+        img: "https://maiscarnesuina.com.br/wp-content/themes/abcs-carne-suina/img/escolha-mais-logo.png",
+        link: "https://www.maiscarnesuina.com.br/",
+        desc: `Criado em wordpress, a meta era aumentar o numero de vendas, tanto pessoas fisicas quanto juridica.`,
+      },
+      {
+        id: 11,
+        title: "Restaura Brasil",
+        category: "Site",
+        price: 16.99,
+        img: "https://www.tnc.org.br/content/dam/tnc/nature/en/logos/tnc-logo-primary-registered-dark-text.svg",
+        link: "https://www.restaurabrasil.org.br/",
+        desc: `Criado em wordpress, a TNC trabalha com restauração florestal no Brasil desde 2001.`,
+      },
+      {
+        id: 12,
+        title: "Celulose Irani",
+        category: "Site",
+        price: 16.99,
+        img: "https://irani.com.br/wp-content/themes/irani.institucional.theme/imgs/logo_irani_papel_e_embalagem.png",
+        link: "https://irani.com.br/",
+        desc: `Criado em wordpress, o objetivo principal era trazer novos distribuidores e empresas interessadas nos produtos da IRANI.`,
+      },
+      {
+        id: 13,
+        title: "CMA Mercados",
+        category: "Site",
+        price: 16.99,
+        img: "https://www.cma.com.br/wp-content/uploads/2024/01/logo-cma.png",
+        link: "https://www.cma.com.br/",
+        desc: `Criado em wordpress, para divulgar os serviços que a empresa atende.`,
+      },
+      {
+        id: 14,
+        title: "Agência CMA",
+        category: "Ecommerce",
+        price: 16.99,
+        img: "https://www.agenciacma.com.br/wp-content/uploads/2024/05/logo-safras-news.png",
+        link: "https://agenciacma.com.br/",
+        desc: `Criado em wordpress, a Agência CMA atua tanto como agência de noticias, quanto como uma loja virtual.`,
+      },
+      {
+        id: 15,
+        title: "Safras & Mercado",
+        category: "Ecommerce",
+        price: 16.99,
+        img: "https://www.cma.com.br/wp-content/uploads/2024/01/logo-safras-fonte-branca.png",
+        link: "https://safras.com.br/",
+        desc: `Criado em wordpress, Safras & Mercado atua tanto como agência de noticias, quanto como uma loja virtual.`,
+      },
+      {
+        id: 15,
+        title: "Clinica Advance",
+        category: "Site",
+        price: 16.99,
+        img: "https://static.wixstatic.com/media/acf9b5_da98c99cf5f44ba4a523a58e39fbbd3f~mv2.png/v1/fill/w_138,h_58,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/clinic-advance-pt-01%20(1).png",
+        link: "https://www.clinicadvance.com.br/",
+        desc: `Criado em Wix, com o objetivo de divulgar a clinica e os produtos disponiveis para agendamento.`,
       },
     ];
       // get parent element
