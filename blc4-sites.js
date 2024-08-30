1453c3308ff6384ad177504e8369c78a7f35809a
Pena <gui.domenes@gmail.com>
2024-08-30 15:58:58 -0300

    att

--- blc4-sites.js
+++ blc4-sites.js
@@ -1,208 +1,217 @@
-
-
-
-
-  /******************************************************** */
+ /******************************************************** */
   /**************   SITES FEITOS   *************** */
 
-    const menu = [
-      {
-        id: 1,
-        title: "Roberta Faustini",
-        category: "Ecommerce",
-        price: 15.99,
-        img: "https://robertasafaustini.com.br/wp-content/uploads/2022/04/LOGO_robertasafaustini_capa-1-scaled-1.jpg",
-        link: "https://robertasafaustini.com.br/shop/",
-        desc: `Criado em Wordpress, esta loja faz a venda de semi joias.`,
-      },
-      {
-        id: 2,
-        title: "Agencia Palmtree",
-        category: "Site",
-        price: 13.99,
-        img: "https://www.agenciapalmtree.com/wp-content/uploads/2024/04/logo-palm-branco.webp",
-        link: "https://agenciapalmtree.com",
-        desc: `Criado em wordpress, para uma agência de marketing, com foco em SEO, para captar leads.`,
-      },
-      {
-        id: 3,
-        title: "Longevitech",
-        category: "Ecommerce",
-        price: 6.99,
-        img: "https://longevitech.com.br/wp-content/uploads/2023/06/Logotipo-Degrade-1400x248.png",
-        link: "https://longevitech.com.br",
-        desc: `Criado em Wordpress, especializado em produtos inteligentes para uma vida melhor com soluções inovadoras em Tecnologia Assistiva que ampliem a independência do indivíduo.`,
-      },
-      {
-        id: 4,
-        title: "MBA EACH USP",
-        category: "Site",
-        price: 20.99,
-        img: "https://mbaeachusp.com/wp-content/uploads/2022/09/Logo-Completo-Branco.png",
-        link: "https://mbaeachusp.com",
-        desc: `Site criado em wordpress, para captura de leads interessadas em cursos MBA.`,
-      },
-      {
-        id: 5,
-        title: "BRA Sports",
-        category: "Site",
-        price: 22.99,
-        img: "https://brasports.com.br/wp-content/uploads/2019/07/bra-400.png",
-        link: "https://brasports.com.br",
-        desc: `Criado em wordpress, este site tem como principal objetivo, o agendamento de quadras esportivas.`,
-      },
-      {
-        id: 6,
-        title: "Petworks",
-        category: "Ecommerce",
-        price: 18.99,
-        img: "https://petworks.com.br/wp-content/uploads/2022/08/pet-works-branco-marrom-1024x601.png",
-        link: "https://petworks.com.br/",
-        desc: `Criado em Wordpress, este site tem como principal, a divulgação dos produtos para pets, lançados pela empresa Petworks.`, 
-      },
-      {
-        id: 7,
-        title: "Escola Superior Contabil Fiscal e RH",
-        category: "Ecommerce",
-        price: 8.99,
-        img: "https://escolasuperioresn.com.br/wp-content/uploads/2023/09/logo-site.png",
-        link: "https://escolasuperioresn.com.br",
-        desc: `Criado em wordpress, este ecommerce faz vendas de cursos para as areas contábil, fiscal e RH.`,
-      },
-      {
-        id: 8,
-        title: "4 Health Alphaville",
-        category: "Site",
-        price: 12.99,
-        img: "https://4healthalphaville.com.br/wp-content/uploads/2024/03/Logo-4Health-qiquedk1fjzmybpb671hp39wxbpbojp50797l74kbg.webp",
-        link: "https://4healthalphaville.com.br",
-        desc: `Criado em Wordpress, com a meta de divulgar os serviços prestados na clinica.`,
-      },
-      {
-        id: 9,
-        title: "Today Corp",
-        category: "Site",
-        price: 16.99,
-        img: "https://www.todaycorp.com.br/wp-content/uploads/2023/04/TodayCorp-Logo-brancoamarelo-1024x573.png",
-        link: "https://todaycorp.com.br/",
-        desc: `Criado em Wordpress, para dilvulgar a marca e os produtos prestados por esta empresa de Eventos.`,
-      },
-      {
-        id: 10,
-        title: "Mais Carne Suína",
-        category: "Ecommerce",
-        price: 16.99,
-        img: "https://maiscarnesuina.com.br/wp-content/themes/abcs-carne-suina/img/escolha-mais-logo.png",
-        link: "https://www.maiscarnesuina.com.br/",
-        desc: `Criado em wordpress, a meta era aumentar o numero de vendas, tanto pessoas fisicas quanto juridica.`,
-      },
-      {
-        id: 11,
-        title: "Restaura Brasil",
-        category: "Site",
-        price: 16.99,
-        img: "https://www.tnc.org.br/content/dam/tnc/nature/en/logos/tnc-logo-primary-registered-dark-text.svg",
-        link: "https://www.restaurabrasil.org.br/",
-        desc: `Criado em wordpress, a TNC trabalha com restauração florestal no Brasil desde 2001.`,
-      },
-      {
-        id: 12,
-        title: "Celulose Irani",
-        category: "Site",
-        price: 16.99,
-        img: "https://irani.com.br/wp-content/themes/irani.institucional.theme/imgs/logo_irani_papel_e_embalagem.png",
-        link: "https://irani.com.br/",
-        desc: `Criado em wordpress, o objetivo principal era trazer novos distribuidores e empresas interessadas nos produtos da IRANI.`,
-      },
-      {
-        id: 13,
-        title: "CMA Mercados",
-        category: "Site",
-        price: 16.99,
-        img: "https://www.cma.com.br/wp-content/uploads/2024/01/logo-cma.png",
-        link: "https://www.cma.com.br/",
-        desc: `Criado em wordpress, para divulgar os serviços que a empresa atende.`,
-      },
-      {
-        id: 14,
-        title: "Agência CMA",
-        category: "Ecommerce",
-        price: 16.99,
-        img: "https://www.agenciacma.com.br/wp-content/uploads/2024/05/logo-safras-news.png",
-        link: "https://agenciacma.com.br/",
-        desc: `Criado em wordpress, a Agência CMA atua tanto como agência de noticias, quanto como uma loja virtual.`,
-      },
-      {
-        id: 15,
-        title: "Safras & Mercado",
-        category: "Ecommerce",
-        price: 16.99,
-        img: "https://www.cma.com.br/wp-content/uploads/2024/01/logo-safras-fonte-branca.png",
-        link: "https://safras.com.br/",
-        desc: `Criado em wordpress, Safras & Mercado atua tanto como agência de noticias, quanto como uma loja virtual.`,
-      },
-      {
-        id: 15,
-        title: "Clinica Advance",
-        category: "Site",
-        price: 16.99,
-        img: "https://static.wixstatic.com/media/acf9b5_da98c99cf5f44ba4a523a58e39fbbd3f~mv2.png/v1/fill/w_138,h_58,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/clinic-advance-pt-01%20(1).png",
-        link: "https://www.clinicadvance.com.br/",
-        desc: `Criado em Wix, com o objetivo de divulgar a clinica e os produtos disponiveis para agendamento.`,
-      },
-    ];
-      // get parent element
-    const sectionCenter = document.querySelector(".section-center");
-    const btnContainer = document.querySelector(".btn-sites");
-    const filterBtns = document.querySelectorAll(".sites-filter-btn");
-    // display all items when page loads
-    window.addEventListener("DOMContentLoaded", function () {
-      diplayMenuItems(menu);
-    });
-  
-    function diplayMenuItems(menuItems) {
-      let displayMenu = menuItems.map(function (item) {
-        console.log('item' + item);
-        return `<article class="menu-item">
-              <a href="${item.link}"><img src=${item.img} alt=${item.title} class="photo" /></a>
-              <div class="sites-item-info">
-                <div class="site-item-titulo">
-                  <span>${item.category}</span>
-                  <h4>${item.title}</h4>
-                </div>
-                <div class="site-item-texto">
-                <p class="sites-item-text">
-                  ${item.desc}
-                </p>
-                <button class="site-item-btn"><a href="${item.link}">Ver Site</a></button>
-                <div>
+  const menu = [
+    {
+      id: 1,
+      title: "Roberta Faustini",
+      category: "Ecommerce",
+      capa: 15.99,
+      img: "https://robertasafaustini.com.br/wp-content/uploads/2022/04/LOGO_robertasafaustini_capa-1-scaled-1.jpg",
+      link: "https://robertasafaustini.com.br/shop/",
+      desc: `Criado em Wordpress, esta loja faz a venda de semi joias.`,
+    },
+    {
+      id: 2,
+      title: "Agencia Palmtree",
+      category: "Site",
+      capa: 13.99,
+      img: "https://www.agenciapalmtree.com/wp-content/uploads/2024/04/logo-palm-branco.webp",
+      link: "https://agenciapalmtree.com",
+      desc: `Criado em wordpress, para uma agência de marketing, com foco em SEO, para captar leads.`,
+    },
+    {
+      id: 3,
+      title: "Longevitech",
+      category: "Ecommerce",
+      capa: 6.99,
+      img: "https://longevitech.com.br/wp-content/uploads/2023/06/Logotipo-Degrade-1400x248.png",
+      link: "https://longevitech.com.br",
+      desc: `Criado em Wordpress, especializado em produtos inteligentes para uma vida melhor com soluções inovadoras em Tecnologia Assistiva que ampliem a independência do indivíduo.`,
+    },
+    {
+      id: 4,
+      title: "MBA EACH USP",
+      category: "Site",
+      capa: 20.99,
+      img: "https://mbaeachusp.com/wp-content/uploads/2022/09/Logo-Completo-Branco.png",
+      link: "https://mbaeachusp.com",
+      desc: `Site criado em wordpress, para captura de leads interessadas em cursos MBA.`,
+    },
+    {
+      id: 5,
+      title: "BRA Sports",
+      category: "Site",
+      capa: 22.99,
+      img: "https://brasports.com.br/wp-content/uploads/2019/07/bra-400.png",
+      link: "https://brasports.com.br",
+      desc: `Criado em wordpress, este site tem como principal objetivo, o agendamento de quadras esportivas.`,
+    },
+    {
+      id: 6,
+      title: "Petworks",
+      category: "Ecommerce",
+      capa: 18.99,
+      img: "https://petworks.com.br/wp-content/uploads/2022/08/pet-works-branco-marrom-1024x601.png",
+      link: "https://petworks.com.br/",
+      desc: `Criado em Wordpress, este site tem como principal, a divulgação dos produtos para pets, lançados pela empresa Petworks.`, 
+    },
+    {
+      id: 7,
+      title: "Escola Superior Contabil Fiscal e RH",
+      category: "Ecommerce",
+      capa: 8.99,
+      img: "https://escolasuperioresn.com.br/wp-content/uploads/2023/09/logo-site.png",
+      link: "https://escolasuperioresn.com.br",
+      desc: `Criado em wordpress, este ecommerce faz vendas de cursos para as areas contábil, fiscal e RH.`,
+    },
+    {
+      id: 8,
+      title: "4 Health Alphaville",
+      category: "Site",
+      capa: 12.99,
+      img: "https://4healthalphaville.com.br/wp-content/uploads/2024/03/Logo-4Health-qiquedk1fjzmybpb671hp39wxbpbojp50797l74kbg.webp",
+      link: "https://4healthalphaville.com.br",
+      desc: `Criado em Wordpress, com a meta de divulgar os serviços prestados na clinica.`,
+    },
+    {
+      id: 9,
+      title: "Today Corp",
+      category: "Site",
+      capa: 16.99,
+      img: "https://www.todaycorp.com.br/wp-content/uploads/2023/04/TodayCorp-Logo-brancoamarelo-1024x573.png",
+      link: "https://todaycorp.com.br/",
+      desc: `Criado em Wordpress, para dilvulgar a marca e os produtos prestados por esta empresa de Eventos.`,
+    },
+    {
+      id: 10,
+      title: "Mais Carne Suína",
+      category: "Ecommerce",
+      capa: 16.99,
+      img: "https://maiscarnesuina.com.br/wp-content/themes/abcs-carne-suina/img/escolha-mais-logo.png",
+      link: "https://www.maiscarnesuina.com.br/",
+      desc: `Criado em wordpress, a meta era aumentar o numero de vendas, tanto pessoas fisicas quanto juridica.`,
+    },
+    {
+      id: 11,
+      title: "Restaura Brasil",
+      category: "Site",
+      capa: 16.99,
+      img: "https://www.tnc.org.br/content/dam/tnc/nature/en/logos/tnc-logo-primary-registered-dark-text.svg",
+      link: "https://www.restaurabrasil.org.br/",
+      desc: `Criado em wordpress, a TNC trabalha com restauração florestal no Brasil desde 2001.`,
+    },
+    {
+      id: 12,
+      title: "Celulose Irani",
+      category: "Site",
+      capa: 16.99,
+      img: "https://irani.com.br/wp-content/themes/irani.institucional.theme/imgs/logo_irani_papel_e_embalagem.png",
+      link: "https://irani.com.br/",
+      desc: `Criado em wordpress, o objetivo principal era trazer novos distribuidores e empresas interessadas nos produtos da IRANI.`,
+    },
+    {
+      id: 13,
+      title: "CMA Mercados",
+      category: "Site",
+      capa: 16.99,
+      img: "https://www.cma.com.br/wp-content/uploads/2024/01/logo-cma.png",
+      link: "https://www.cma.com.br/",
+      desc: `Criado em wordpress, para divulgar os serviços que a empresa atende.`,
+    },
+    {
+      id: 14,
+      title: "Agência CMA",
+      category: "Ecommerce",
+      capa: 16.99,
+      img: "https://www.agenciacma.com.br/wp-content/uploads/2024/05/logo-safras-news.png",
+      link: "https://agenciacma.com.br/",
+      desc: `Criado em wordpress, a Agência CMA atua tanto como agência de noticias, quanto como uma loja virtual.`,
+    },
+    {
+      id: 15,
+      title: "Safras & Mercado",
+      category: "Ecommerce",
+      capa: 16.99,
+      img: "https://www.cma.com.br/wp-content/uploads/2024/01/logo-safras-fonte-branca.png",
+      link: "https://safras.com.br/",
+      desc: `Criado em wordpress, Safras & Mercado atua tanto como agência de noticias, quanto como uma loja virtual.`,
+    },
+    {
+      id: 15,
+      title: "Clinica Advance",
+      category: "Site",
+      capa: "img/irani-capa-mb.png",
+      img: "https://static.wixstatic.com/media/acf9b5_da98c99cf5f44ba4a523a58e39fbbd3f~mv2.png/v1/fill/w_138,h_58,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/clinic-advance-pt-01%20(1).png",
+      link: "https://www.clinicadvance.com.br/",
+      desc: `Criado em Wix, com o objetivo de divulgar a clinica e os produtos disponiveis para agendamento.`,
+    },
+  ];
+    // get parent element
+  const sectionCenter = document.querySelector(".section-center");
+  const btnContainer = document.querySelector(".btn-sites");
+  const filterBtns = document.querySelectorAll(".sites-filter-btn");
+  // display all items when page loads
+  window.addEventListener("DOMContentLoaded", function () {
+    diplayMenuItems(menu);
+  });
+
+  function diplayMenuItems(menuItems) {
+    let displayMenu = menuItems.map(function (item) {
+      console.log('item' + item);
+      return `        
+      <section class="section-gap info-area" id="about">
+      <div class="container">				
+        <div class="single-info row mt-40 align-items-center">
+          <div class="col-lg-6 col-md-12 text-center no-padding info-left">
+            <div class="info-thumb">
+              <img src=${item.capa} alt=${item.title} class="img-fluid info-img" alt="">
+            </div>
+          </div>
+          <div class="col-lg-6 col-md-12 no-padding info-rigth">
+            <div class="info-content">
+              <h2 class="pb-30 titutlo-site">Dr. Travor James</h2>
+              <p>
+                inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards – especially in the workplace. That’s why it’s crucial that, as women.								
+              </p>
+              <br>
+              <p>
+                inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards – especially in the workplace. That’s why it’s crucial that, as women. inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards – especially in the workplace. That’s why it’s crucial that, as women.									
+              </p>
+              <br>
+              <img class="site-lg" src=${item.img} alt=${item.title}>
               </div>
-            </article>`;
-      });
-      displayMenu = displayMenu.join("");
-      console.log('item' + displayMenu);
-      sectionCenter.innerHTML = displayMenu;
-    }
-  
-  
-  
-    filterBtns.forEach(function (btn) {
-      btn.addEventListener("click", function (e) {
-        const category = e.currentTarget.dataset.id;
-  
-        const menuCategory = menu.filter(function (menuItem) {
-          console.log(menuItem.category);
-          if (menuItem.category === category) {
-            return menuItem;
-          }
-        });
-  
-        if (category === "all") {
-          diplayMenuItems(menu);
-        } else {
-          diplayMenuItems(menuCategory);
+          </div>
+        </div>
+      </div>
+    </section>`
+          
+          ;
+    });
+    displayMenu = displayMenu.join("");
+    console.log('item' + displayMenu);
+    sectionCenter.innerHTML = displayMenu;
+  }
+
+
+
+  filterBtns.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+      const category = e.currentTarget.dataset.id;
+
+      const menuCategory = menu.filter(function (menuItem) {
+        console.log(menuItem.category);
+        if (menuItem.category === category) {
+          return menuItem;
         }
       });
+
+      if (category === "all") {
+        diplayMenuItems(menu);
+      } else {
+        diplayMenuItems(menuCategory);
+      }
     });
-      /**************   SITES FEITOS   *************** */
-      /************************************************ */
+  });
+    /**************   SITES FEITOS   *************** */
+    /************************************************ */
