1baa341ee51b9f24858cdee7dd6516e9f1d88052
Guilherme Domenes Santana <guilherme.santana@clude.com.br>
2024-09-11 09:50:15 -0300

    alterado

--- header.js
+++ header.js
@@ -1,7 +1,7 @@
 const btnsmenu = document.getElementById('menu-btn');
-const menunav = document.getElementById('navigation');
+const menunav = document.getElementById('navbar');
 
 btnsmenu.addEventListener('click', () => {
-    btnsmenu.classList.toggle('active');
     menunav.classList.toggle('active');
+    btnsmenu.classList.toggle('active');
 })
